aa2ae0d0cc2e51f253615e0760257783f370d86d
Alejandro-666 <moisesrattia2@hotmail.com>
2023-11-30 22:39:03 -0500

    .

--- projects/mi primera app/src/main.jsx
+++ projects/mi primera app/src/main.jsx
@@ -1,8 +1,9 @@
-
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-
-)
+const root = ReactDOM.createRoot(document.getElementById
+  ('root'))
+  
+  root.render(<button>hola button</button>)
